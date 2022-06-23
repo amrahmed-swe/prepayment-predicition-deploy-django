d11ea7395371e6511af76abfb0e5729a0c5b037b
Amr Ahmed <52863221+amrahmed-swe@users.noreply.github.com>
2022-06-23 18:26:19 +0200

    Add files via upload

--- LoanDjango/settings.py
+++ LoanDjango/settings.py
@@ -41,6 +41,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -118,6 +119,9 @@ USE_TZ = True
 STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static/"),
+)
+STATIC_ROOT = (
+    os.path.join(BASE_DIR, "staticfiles"),
 )
 
 # Default primary key field type
